126af0df686845bef510081a81c2c411633801d5
pratxf <mehhprat@gmail.com>
2026-05-10 23:51:32 +0530

    remove live support pill from marketing pages

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,15 +1,9 @@
 import { LenisProvider } from "@/components/lenis-provider";
-import LiveSupportPill from "@/components/landing/LiveSupportPill";
 
 export default function MarketingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <LenisProvider>
-      {children}
-      <LiveSupportPill />
-    </LenisProvider>
-  );
+  return <LenisProvider>{children}</LenisProvider>;
 }
